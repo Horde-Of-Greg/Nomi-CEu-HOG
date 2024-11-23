cfe22f347ca6f7073b2470573919589c175375d3
D-Alessian <alessian@alessian.be>
2024-11-23 20:00:30 +0100

    Make a V1 of a script to remove useless small/tiny dust packager recipe and remove the crafting recipes for them.
    Signed-off-by: D-Alessian <alessian@alessian.be>

---
+++ overrides/groovy/postInit/Post-Initial/Main/General/Misc/smallAndTinyDusts.groovy
@@ -0,0 +1,146 @@
+/*
+Small Dusts
+Put here any small dusts to remove that dont serve any purpose at all.
+Make sure these dusts are not useful for any recycling recipe.
+If a dust here is an output or an input of any recipe, it is a bug and should get reported
+*/
+for (var smallToRemove : [
+	//List Materials here - Ordered by ID
+	//Nomilabs
+	'Ardite',
+	'Manyullyn',
+	'CrystalMatrix',
+	'Soularium',
+	'Iridosmine8020',
+	'Kaemanite',
+	'TungstenTrioxide',
+	'NiobiumPentoxide',
+	'TantalumPentoxide',
+	'Fluorite',
+	'ManganeseDifluoride',
+	'MolybdenumTrioxide',
+	'LeadChloride',
+	'Wollastonite',
+	'SodiumMetavanadate',
+	'VanadiumPentoxide',
+	'AmmoniumMetavanadate',
+	'PhthalicAnhydride',
+	'Ethylanthraquinone',
+	'GrapheneOxide',
+	'KaptonK',
+	'Durene',
+	'PyromelliticDianhydride',
+	'Oxydianiline',
+	'NaquadahOxide',
+	'Pyromorphite',
+	'NaquadahHydroxide',
+	'Snowchestite',
+	'CaesiumHydroxide',
+	'LeadMetasilicate',
+	'PlatinumMetallic',
+	'PalladiumMetallic',
+	'AmmoniumHexachloroplatinate',
+	'PotassiumBisulfate',
+	'PotassiumPyrosulfate',
+	'PotassiumSulfate',
+	'ZincSulfate',
+	'SodiumNitrate',
+	'RhodiumNitrate',
+	'SodiumRuthenate',
+	'SodiumPeroxide',
+	'IridiumDioxideResidue',
+	'AmmoniumHexachloroiridiate',
+	'CrudePlatinumResidue',
+	'CrudePalladiumResidue',
+	'IridiumGroupSludge',
+	'CrudeRhodiumResidue',
+	'RhodiumSalt',
+	'SodiumMethoxide',
+	'StoneResidue',
+	'UncommonResidue',
+	'OxidisedResidue',
+	'RefinedResidue',
+	'Taranium',
+	'Darmstadtite',
+	'Dulysite',
+	'Laurite',
+	'Cuprorhodsite'
+])
+{
+	//Hides the crafting recipe
+	mods.jei.ingredient.removeAndHide(metaitem('nomilabs:dustSmall' + smallToRemove))
+
+	//Removes the packager recipes
+	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dustSmall' + smallToRemove) * 4, metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
+	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dust' + smallToRemove), metaitem('circuit.integrated').withNbt(['Configuration': 2])], null)
+}
+
+/*
+Tiny Dusts
+Same as above but for tiny dusts.
+Any material with an ingot form should immediately be excluded because nuggets (maybe remove the nugget?)
+*/
+for (var tinyToRemove : [
+	//List Materials here - Ordered by ID
+	//Nomilabs
+	'Osmiridium8020',
+	'Iridosmine8020',
+	'Kaemanite',
+	'TungstenTrioxide',
+	'NiobiumPentoxide',
+	'TantalumPentoxide',
+	'Fluorite',
+	'ManganeseDifluoride',
+	'MolybdenumTrioxide',
+	'LeadChloride',
+	'Wollastonite',
+	'SodiumMetavanadate',
+	'AmmoniumMetavanadate',
+	'PhthalicAnhydride',
+	'Ethylanthraquinone',
+	'GrapheneOxide',
+	'Durene',
+	'PyromelliticDianhydride',
+	'Oxydianiline',
+	'NaquadahOxide',
+	'Pyromorphite',
+	'NaquadahHydroxide',
+	'Snowchestite',
+	'CaesiumHydroxide',
+	'LeadMetasilicate',
+	'PlatinumMetallic',
+	'PalladiumMetallic',
+	'AmmoniumHexachloroplatinate',
+	'PotassiumBisulfate',
+	'PotassiumPyrosulfate',
+	'PotassiumSulfate',
+	'ZincSulfate',
+	'SodiumNitrate',
+	'RhodiumNitrate',
+	'SodiumRuthenate',
+	'SodiumPeroxide',
+	'IridiumDioxideResidue',
+	'AmmoniumHexachloroiridiate',
+	'CrudePlatinumResidue',
+	'CrudePalladiumResidue',
+	'IridiumGroupSludge',
+	'CrudeRhodiumResidue',
+	'RhodiumSalt',
+	'StoneResidue',
+	'UncommonResidue',
+	'OxidisedResidue',
+	'RefinedResidue',
+	'Darmstadtite',
+	'Dulysite',
+	'Laurite',
+	'Cuprorhodsite'
+])
+{
+	//Hides the crafting recipe
+	mods.jei.ingredient.removeAndHide(metaitem('nomilabs:dustTiny' + tinyToRemove))
+
+	//Removes the packager recipes
+	mods.gregtech.packer.removeByInput(12, [metaitem('dustTiny' + tinyToRemove) * 9, metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
+	mods.gregtech.packer.removeByInput(12, [metaitem('dust' + tinyToRemove), metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
+
+}
