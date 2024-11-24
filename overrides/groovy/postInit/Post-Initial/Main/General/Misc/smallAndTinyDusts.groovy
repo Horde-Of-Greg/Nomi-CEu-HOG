6e8651afc816a53d4a8bce380f7486c2dccb591c
D-Alessian <alessian@alessian.be>
2024-11-24 19:04:58 +0100

    Add a list for Both small & tiny Doing this results in less lines and therefore it should be lighter.
    Signed-off-by: D-Alessian <alessian@alessian.be>

--- overrides/groovy/postInit/Post-Initial/Main/General/Misc/smallAndTinyDusts.groovy
+++ overrides/groovy/postInit/Post-Initial/Main/General/Misc/smallAndTinyDusts.groovy
@@ -1,149 +1,113 @@
+// NOMILABS
+
 /*
-Small Dusts - Nomilabs
-Put here any small dusts to remove that dont serve any purpose at all.
+BOTH DUSTS
+Put here any material for which both small and tiny dusts dont serve any purpose at all.
 Make sure these dusts are not useful for any recycling recipe.
-If a dust here is an output or an input of any recipe, it is a bug and should get reported
-*/
-for (var smallToRemove : [
-	//List Materials here - Ordered by ID
-	//Nomilabs
-	'Ardite',
-	'Manyullyn',
-	'CrystalMatrix',
-	'Soularium',
-	'Iridosmine8020',
-	'Kaemanite',
-	'TungstenTrioxide',
-	'NiobiumPentoxide',
-	'TantalumPentoxide',
-	'Fluorite',
-	'ManganeseDifluoride',
-	'MolybdenumTrioxide',
-	'LeadChloride',
-	'Wollastonite',
-	'SodiumMetavanadate',
-	'VanadiumPentoxide',
-	'AmmoniumMetavanadate',
-	'PhthalicAnhydride',
-	'Ethylanthraquinone',
-	'GrapheneOxide',
-	'KaptonK',
-	'Durene',
-	'PyromelliticDianhydride',
-	'Oxydianiline',
-	'NaquadahOxide',
-	'Pyromorphite',
-	'NaquadahHydroxide',
-	'Snowchestite',
-	'CaesiumHydroxide',
-	'LeadMetasilicate',
-	'PlatinumMetallic',
-	'PalladiumMetallic',
-	'AmmoniumHexachloroplatinate',
-	'PotassiumBisulfate',
-	'PotassiumPyrosulfate',
-	'PotassiumSulfate',
-	'ZincSulfate',
-	'SodiumNitrate',
-	'RhodiumNitrate',
-	'SodiumRuthenate',
-	'SodiumPeroxide',
-	'IridiumDioxideResidue',
-	'AmmoniumHexachloroiridiate',
-	'CrudePlatinumResidue',
-	'CrudePalladiumResidue',
-	'IridiumGroupSludge',
-	'CrudeRhodiumResidue',
-	'RhodiumSalt',
-	'SodiumMethoxide',
-	'StoneResidue',
-	'UncommonResidue',
-	'OxidisedResidue',
-	'RefinedResidue',
-	'Taranium',
-	'Darmstadtite',
-	'Dulysite',
-	'Laurite',
-	'Cuprorhodsite'
-])
-{
+ */
+[	//List Materials here - Ordered by ID
+	 'Iridosmine8020',
+	 'Kaemanite',
+	 'TungstenTrioxide',
+	 'NiobiumPentoxide',
+	 'TantalumPentoxide',
+	 'Fluorite',
+	 'ManganeseDifluoride',
+	 'MolybdenumTrioxide',
+	 'LeadChloride',
+	 'Wollastonite',
+	 'SodiumMetavanadate',
+	 'AmmoniumMetavanadate',
+	 'PhthalicAnhydride',
+	 'Ethylanthraquinone',
+	 'GrapheneOxide',
+	 'Durene',
+	 'PyromelliticDianhydride',
+	 'Oxydianiline',
+	 'NaquadahOxide',
+	 'Pyromorphite',
+	 'NaquadahHydroxide',
+	 'Snowchestite',
+	 'CaesiumHydroxide',
+	 'LeadMetasilicate',
+	 'PlatinumMetallic',
+	 'PalladiumMetallic',
+	 'AmmoniumHexachloroplatinate',
+	 'PotassiumBisulfate',
+	 'PotassiumPyrosulfate',
+	 'PotassiumSulfate',
+	 'ZincSulfate',
+	 'SodiumNitrate',
+	 'RhodiumNitrate',
+	 'SodiumRuthenate',
+	 'SodiumPeroxide',
+	 'IridiumDioxideResidue',
+	 'AmmoniumHexachloroiridiate',
+	 'CrudePlatinumResidue',
+	 'CrudePalladiumResidue',
+	 'IridiumGroupSludge',
+	 'CrudeRhodiumResidue',
+	 'RhodiumSalt',
+	 'StoneResidue',
+	 'UncommonResidue',
+	 'OxidisedResidue',
+	 'RefinedResidue',
+	 'Darmstadtite',
+	 'Dulysite',
+	 'Laurite',
+	 'Cuprorhodsite'
+].each { var material ->
 	//Removes the crafting recipe
-	mods.jei.ingredient.removeAndHide(metaitem('nomilabs:dustSmall' + smallToRemove))
-	crafting.removeByInput(metaitem('nomilabs:dustSmall' + smallToRemove))
+	mods.jei.ingredient.removeAndHide(metaitem('nomilabs:dustSmall' + material))
+	mods.jei.ingredient.removeAndHide(metaitem('nomilabs:dustTiny' + material))
+	crafting.removeByInput(metaitem('nomilabs:dustSmall' + material))
+	crafting.removeByInput(metaitem('nomilabs:dustTiny' + material))
+
 
 	//Removes the packager recipes
-	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dustSmall' + smallToRemove) * 4, metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
-	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dust' + smallToRemove), metaitem('circuit.integrated').withNbt(['Configuration': 2])], null)
+	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dustSmall' + material) * 4, metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
+	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dustTiny' + material) * 9, metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
+	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dust' + material), metaitem('circuit.integrated').withNbt(['Configuration': 2])], null)
+	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dust' + material), metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
 }
 
 /*
-Tiny Dusts - Nomilabs
-Same as above but for tiny dusts.
-Any material with an ingot form should immediately be excluded because nuggets (maybe remove the nugget?)
+SMALL DUSTS
+Put here materials that need only their small dust removed but not tiny dust
 */
-for (var tinyToRemove : [
-	//List Materials here - Ordered by ID
-	//Nomilabs
-	'Osmiridium8020',
-	'Iridosmine8020',
-	'Kaemanite',
-	'TungstenTrioxide',
-	'NiobiumPentoxide',
-	'TantalumPentoxide',
-	'Fluorite',
-	'ManganeseDifluoride',
-	'MolybdenumTrioxide',
-	'LeadChloride',
-	'Wollastonite',
-	'SodiumMetavanadate',
-	'AmmoniumMetavanadate',
-	'PhthalicAnhydride',
-	'Ethylanthraquinone',
-	'GrapheneOxide',
-	'Durene',
-	'PyromelliticDianhydride',
-	'Oxydianiline',
-	'NaquadahOxide',
-	'Pyromorphite',
-	'NaquadahHydroxide',
-	'Snowchestite',
-	'CaesiumHydroxide',
-	'LeadMetasilicate',
-	'PlatinumMetallic',
-	'PalladiumMetallic',
-	'AmmoniumHexachloroplatinate',
-	'PotassiumBisulfate',
-	'PotassiumPyrosulfate',
-	'PotassiumSulfate',
-	'ZincSulfate',
-	'SodiumNitrate',
-	'RhodiumNitrate',
-	'SodiumRuthenate',
-	'SodiumPeroxide',
-	'IridiumDioxideResidue',
-	'AmmoniumHexachloroiridiate',
-	'CrudePlatinumResidue',
-	'CrudePalladiumResidue',
-	'IridiumGroupSludge',
-	'CrudeRhodiumResidue',
-	'RhodiumSalt',
-	'StoneResidue',
-	'UncommonResidue',
-	'OxidisedResidue',
-	'RefinedResidue',
-	'Darmstadtite',
-	'Dulysite',
-	'Laurite',
-	'Cuprorhodsite'
-])
-{
+[	//List Materials here - Ordered by ID
+	 'Ardite',
+	 'Manyullyn',
+	 'CrystalMatrix',
+	 'Soularium',
+	 'VanadiumPentoxide',
+	 'KaptonK',
+	 'SodiumMethoxide',
+	 'Taranium'
+].each { var material ->
 	//Removes the crafting recipe
-	mods.jei.ingredient.removeAndHide(metaitem('nomilabs:dustTiny' + tinyToRemove))
-	crafting.removeByInput(metaitem('nomilabs:dustTiny' + tinyToRemove))
+	mods.jei.ingredient.removeAndHide(metaitem('nomilabs:dustSmall' + material))
+	crafting.removeByInput(metaitem('nomilabs:dustSmall' + material))
 
 	//Removes the packager recipes
-	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dustTiny' + tinyToRemove) * 9, metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
-	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dust' + tinyToRemove), metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
+	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dustSmall' + material) * 4, metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
+	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dust' + material), metaitem('circuit.integrated').withNbt(['Configuration': 2])], null)
+}
 
+/*
+TINY DUSTS
+Put here materials that need only their tiny dust removed but not small dust
+Any material with an ingot form should immediately be excluded because nuggets (maybe remove the nugget?)
+*/
+[	//List Materials here - Ordered by ID
+	'Osmiridium8020',
+].each { var material ->
+	//Removes the crafting recipe
+	mods.jei.ingredient.removeAndHide(metaitem('nomilabs:dustTiny' + material))
+	crafting.removeByInput(metaitem('nomilabs:dustTiny' + material))
+
+	//Removes the packager recipes
+	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dustTiny' + material) * 9, metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
+	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dust' + material), metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
 }
 
