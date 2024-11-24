4ea9145c07081a13fae1c3173f9e99ec7c9e0eb3
D-Alessian <alessian@alessian.be>
2024-11-24 21:17:05 +0100

    Implement a method to remove dusts to shave off some bytes to the document and overall have it be cleaner
    Signed-off-by: D-Alessian <alessian@alessian.be>

--- overrides/groovy/postInit/Post-Initial/Main/General/Misc/smallAndTinyDusts.groovy
+++ overrides/groovy/postInit/Post-Initial/Main/General/Misc/smallAndTinyDusts.groovy
@@ -1,113 +1,103 @@
 // NOMILABS
 
-/*
-BOTH DUSTS
-Put here any material for which both small and tiny dusts dont serve any purpose at all.
-Make sure these dusts are not useful for any recycling recipe.
- */
-[	//List Materials here - Ordered by ID
-	 'Iridosmine8020',
-	 'Kaemanite',
-	 'TungstenTrioxide',
-	 'NiobiumPentoxide',
-	 'TantalumPentoxide',
-	 'Fluorite',
-	 'ManganeseDifluoride',
-	 'MolybdenumTrioxide',
-	 'LeadChloride',
-	 'Wollastonite',
-	 'SodiumMetavanadate',
-	 'AmmoniumMetavanadate',
-	 'PhthalicAnhydride',
-	 'Ethylanthraquinone',
-	 'GrapheneOxide',
-	 'Durene',
-	 'PyromelliticDianhydride',
-	 'Oxydianiline',
-	 'NaquadahOxide',
-	 'Pyromorphite',
-	 'NaquadahHydroxide',
-	 'Snowchestite',
-	 'CaesiumHydroxide',
-	 'LeadMetasilicate',
-	 'PlatinumMetallic',
-	 'PalladiumMetallic',
-	 'AmmoniumHexachloroplatinate',
-	 'PotassiumBisulfate',
-	 'PotassiumPyrosulfate',
-	 'PotassiumSulfate',
-	 'ZincSulfate',
-	 'SodiumNitrate',
-	 'RhodiumNitrate',
-	 'SodiumRuthenate',
-	 'SodiumPeroxide',
-	 'IridiumDioxideResidue',
-	 'AmmoniumHexachloroiridiate',
-	 'CrudePlatinumResidue',
-	 'CrudePalladiumResidue',
-	 'IridiumGroupSludge',
-	 'CrudeRhodiumResidue',
-	 'RhodiumSalt',
-	 'StoneResidue',
-	 'UncommonResidue',
-	 'OxidisedResidue',
-	 'RefinedResidue',
-	 'Darmstadtite',
-	 'Dulysite',
-	 'Laurite',
-	 'Cuprorhodsite'
-].each { var material ->
-	//Removes the crafting recipe
-	mods.jei.ingredient.removeAndHide(metaitem('nomilabs:dustSmall' + material))
-	mods.jei.ingredient.removeAndHide(metaitem('nomilabs:dustTiny' + material))
-	crafting.removeByInput(metaitem('nomilabs:dustSmall' + material))
-	crafting.removeByInput(metaitem('nomilabs:dustTiny' + material))
+def bothLabs = [
+	//List Materials here - Ordered by ID
+	'Iridosmine8020',
+	'Kaemanite',
+	'TungstenTrioxide',
+	'NiobiumPentoxide',
+	'TantalumPentoxide',
+	'Fluorite',
+	'ManganeseDifluoride',
+	'MolybdenumTrioxide',
+	'LeadChloride',
+	'Wollastonite',
+	'SodiumMetavanadate',
+	'AmmoniumMetavanadate',
+	'PhthalicAnhydride',
+	'Ethylanthraquinone',
+	'GrapheneOxide',
+	'Durene',
+	'PyromelliticDianhydride',
+	'Oxydianiline',
+	'NaquadahOxide',
+	'Pyromorphite',
+	'NaquadahHydroxide',
+	'Snowchestite',
+	'CaesiumHydroxide',
+	'LeadMetasilicate',
+	'PlatinumMetallic',
+	'PalladiumMetallic',
+	'AmmoniumHexachloroplatinate',
+	'PotassiumBisulfate',
+	'PotassiumPyrosulfate',
+	'PotassiumSulfate',
+	'ZincSulfate',
+	'SodiumNitrate',
+	'RhodiumNitrate',
+	'SodiumRuthenate',
+	'SodiumPeroxide',
+	'IridiumDioxideResidue',
+	'AmmoniumHexachloroiridiate',
+	'CrudePlatinumResidue',
+	'CrudePalladiumResidue',
+	'IridiumGroupSludge',
+	'CrudeRhodiumResidue',
+	'RhodiumSalt',
+	'StoneResidue',
+	'UncommonResidue',
+	'OxidisedResidue',
+	'RefinedResidue',
+	'Darmstadtite',
+	'Dulysite',
+	'Laurite',
+	'Cuprorhodsite'
+]
 
+def smallLabs = [
+	//List Materials here - Ordered by ID
+	'Ardite',
+	'Manyullyn',
+	'CrystalMatrix',
+	'Soularium',
+	'VanadiumPentoxide',
+	'KaptonK',
+	'SodiumMethoxide',
+	'Taranium'
+]
 
-	//Removes the packager recipes
-	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dustSmall' + material) * 4, metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
-	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dustTiny' + material) * 9, metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
-	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dust' + material), metaitem('circuit.integrated').withNbt(['Configuration': 2])], null)
-	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dust' + material), metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
-}
-
-/*
-SMALL DUSTS
-Put here materials that need only their small dust removed but not tiny dust
-*/
-[	//List Materials here - Ordered by ID
-	 'Ardite',
-	 'Manyullyn',
-	 'CrystalMatrix',
-	 'Soularium',
-	 'VanadiumPentoxide',
-	 'KaptonK',
-	 'SodiumMethoxide',
-	 'Taranium'
-].each { var material ->
-	//Removes the crafting recipe
-	mods.jei.ingredient.removeAndHide(metaitem('nomilabs:dustSmall' + material))
-	crafting.removeByInput(metaitem('nomilabs:dustSmall' + material))
-
-	//Removes the packager recipes
-	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dustSmall' + material) * 4, metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
-	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dust' + material), metaitem('circuit.integrated').withNbt(['Configuration': 2])], null)
-}
-
-/*
-TINY DUSTS
-Put here materials that need only their tiny dust removed but not small dust
-Any material with an ingot form should immediately be excluded because nuggets (maybe remove the nugget?)
-*/
-[	//List Materials here - Ordered by ID
+def tinyLabs = [
+	//List Materials here - Ordered by ID
 	'Osmiridium8020'
-].each { var material ->
+]
+
+// Define the method to remove dusts
+public static void yeetDusts(String size, String modPrefix, String material) {
+	def circuitMap = [
+		'Tiny': 1,
+		'Small': 2
+	]
+	def amountMap = [
+		'Tiny': 9,
+		'Small': 4
+	]
 	//Removes the crafting recipe
-	mods.jei.ingredient.removeAndHide(metaitem('nomilabs:dustTiny' + material))
-	crafting.removeByInput(metaitem('nomilabs:dustTiny' + material))
+	mods.jei.ingredient.removeAndHide(metaitem(modPrefix + 'dust' + size + material))
+	crafting.removeByInput(metaitem(modPrefix + 'dust' + size + material))
 
 	//Removes the packager recipes
-	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dustTiny' + material) * 9, metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
-	mods.gregtech.packer.removeByInput(12, [metaitem('nomilabs:dust' + material), metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
+	mods.gregtech.packer.removeByInput(12, [metaitem(modPrefix + 'dust' + size + material) * amountMap[size], metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
+	mods.gregtech.packer.removeByInput(12, [metaitem(modPrefix + 'dust' + material), metaitem('circuit.integrated').withNbt(['Configuration': circuitMap[size]])], null)
 }
 
+// Execute the methods for each list
+for (material in bothLabs) {
+	yeetDusts('Small', 'nomilabs:', material)
+	yeetDusts('Tiny', 'nomilabs:', material)
+}
+for (material in smallLabs) {
+	yeetDusts('Small', 'nomilabs:', material)
+}
+for (material in tinyLabs) {
+	yeetDusts('Tiny', 'nomilabs:', material)
+}
